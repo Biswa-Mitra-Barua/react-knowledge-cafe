c72550be6a235ff0b40d4c7156131eecf06431de
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-10-22 17:06:06 +0600

    added the bookmarked blogs part

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,26 +1,32 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from "react-icons/fa";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
     const {title,cover,author,author_img, posted_date, reading_time, 
         hashtags
     } = blog;
     return (
-        <div>
-            <img className='w-2/3' src={cover} alt={`Cover picture of the title ${title}`} />
+        <div className='mb-16 mt-6'>
+            <img className='w-full h-96 rounded-md' src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between items-center'>
-                <div className='flex items-center gap-4 mt-4'>
+                <div className='flex items-center mt-6 space-y-3'>
                     <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
-                    <div>
-                        <h3 className='text-2xl'>{author}</h3>
+                    <div className='ml-4'>
+                        <h3 className='text-xl font-semibold'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex items-center gap-2'>
                     <span>{reading_time} min read</span>
+                    <button
+                        onClick={() => handleAddToBookmark(blog)}
+                        className='text-yellow-500 text-xl'>
+                        <FaRegBookmark></FaRegBookmark>
+                    </button>
                 </div>
             </div>
-            <h2 className="text-4xl">{title}</h2>
-            <p>
+            <h2 className="text-3xl mt-4 font-semibold">{title}</h2>
+            <p className='mt-2 text-gray-500'>
                 {
                     hashtags.map((hash,idx) => <span key={idx}><a href="">{hash} </a></span>)
                 }
@@ -30,6 +36,7 @@ const Blog = ({ blog }) => {
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func
 }
 export default Blog;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types';
+import Bookmark from '../Bookmark/Bookmark';
 
-
-const Bookmarks = () => {
+const Bookmarks = ({bookmarks}) => {
     return (
-        <div className="w-1/3">
-            <h1>This is Bookmark Container</h1>
+        <div className="w-1/3 bg-gray-400 ml-5 mt-5 rounded-xl">
+            <h1 className='text-2xl text-center mt-5'>Bookmarks Blogs: {bookmarks.length}</h1>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
+
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array
+}
 
 export default Bookmarks;
