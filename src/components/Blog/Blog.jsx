16debbae2d6b8bd2404b3ac9ee882420fbeaecdd
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-10-22 15:37:07 +0600

    added some properties

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,30 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const {title,cover,author,author_img, posted_date, reading_time, 
+        hashtags
+    } = blog;
     return (
         <div>
-
+            <img className='w-2/3' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-4 mt-4'>
+                    <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash,idx) => <span key={idx}><a href="">{hash} </a></span>)
+                }
+            </p>
         </div>
     );
 };
